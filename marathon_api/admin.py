c1c97ef63bccc120fb5e52cf2a57f5a4636ba7c6
Bakyt <isimovabakyt@gmail.com>
2019-07-17 19:02:41 +0600

    Admin views

--- marathon_api/admin.py
+++ marathon_api/admin.py
@@ -1,3 +1,5 @@
+from .models import Event, Photo
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Photo)
--- marathon_api/models.py
+++ marathon_api/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,9 +11,10 @@ class Event(models.Model):
 
 class Photo(models.Model):
     urlOriginal = models.CharField(max_length=1000)
-    urlCompressed = models.CharField(max_lenth=1000)
+    urlCompressed = models.CharField(max_length=1000)
     watermark = models.CharField(max_length=100)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    photographer=models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{}: {}, {}, {}'.format(self.id, self.urlOriginal, self.watermark, self.event)
