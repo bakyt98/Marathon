7ea0c8734902dc58033ac278df040c45509fdcd2
aidact <aida.ct127@gmail.com>
2019-07-17 16:27:14 +0600

    models added

--- marathon_api/models.py
+++ marathon_api/models.py
@@ -1,3 +1,19 @@
-from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.name)
+
+
+class Photo(models.Model):
+    urlOriginal = models.CharField(max_length=1000)
+    urlCompressed = models.CharField(max_lenth=1000)
+    watermark = models.CharField(max_length=100)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}: {}, {}, {}'.format(self.id, self.urlOriginal, self.watermark, self.event)
