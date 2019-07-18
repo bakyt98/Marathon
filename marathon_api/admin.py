dfa2339d4c26befb6630f8193b1604a65b3c81e4
aidact <aida.ct127@gmail.com>
2019-07-18 13:40:40 +0600

    Permissions added

--- marathon_api/admin.py
+++ marathon_api/admin.py
@@ -4,8 +4,17 @@ from marathon_api.models import Photo, Event
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'original', 'watermark', 'event')
+    list_display = ('id', 'original', 'watermark', 'event', 'created_by')
     fields = ('original', 'watermark', 'event')
+
+    def has_add_permission(self, request, obj=None):
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
 
 
 @admin.register(Event)
